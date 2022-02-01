c6db67cdeb6494ca7ff64fdbcdb5cb86d2fa7678
josephchait <danchait10@gmail.com>
2022-02-01 11:32:37 -0500

    Basic GUI Rdy

--- Python Calculator/main.py
+++ Python Calculator/main.py
@@ -1,15 +1,71 @@
 #this is the main function
-
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+from math_functions import *
 
 # declare the window
-window = Tk()
+window = tk.Tk()
 
 # set window title
 window.title("DS Calculator")
 
 # set window width and height
-window.configure(width=500, height=500)
+window.geometry("500x500")
+
+
+#function that displays a message with the answer
+def show_answer():
+
+    a = 100
+    b = 20
+
+    toShow = "null"
+
+    if(selected_function.get() == "Add"):
+        toShow = "The answer is " + str(add(a,b))
+
+    if(selected_function.get() == "Subtract"):
+        toShow = "The answer is " + str(subtract(a,b))
+    
+    if(selected_function.get() == "Multiply"):
+        toShow = "The answer is " + str(multiply(a,b))
+
+    if(selected_function.get() == "Divide"):
+        toShow = "The answer is " + str(divide(a,b))
+
+    if(selected_function.get() == "Power"):
+        toShow = "The answer is " + str(pow(a,b))
+
+    showinfo(
+        title = "Result",
+        message = toShow
+        
+    )
+
+
+functions = ["Add", "Subtract",  "Multiply" , "Divide", "Power"]
+
+selected_function = tk.StringVar()
+
+# radio buttons
+for function in functions:
+    r = ttk.Radiobutton(
+        window,
+        text = function,
+        value = function,
+        variable = selected_function
+    )
+    r.pack(fill='x', padx=5, pady=5)
+
+
+# button
+button = ttk.Button(
+    window,
+    text = "Get the answer",
+    command = show_answer)
+
+button.pack(fill='x', padx=5, pady=5)
 
 # set window background color
 window.configure(bg='lightgray')
