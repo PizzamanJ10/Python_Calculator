777b894a5b9f54a7777c50605820edb554a4d92e
PizzamanJ10 <55109111+PizzamanJ10@users.noreply.github.com>
2022-02-01 10:24:22 -0500

    Add files via upload

---
+++ Python Calculator/math_functions.py
@@ -0,0 +1,30 @@
+
+
+
+#add function, adds a and b
+def add(a, b):
+    return 0
+
+#subtract function, subtracts b from a
+def subtract(a, b):
+    return 0
+
+#divide function, divides a by b
+def divide(a, b):
+    return 0
+
+#multiply function, multiplies b by a
+def multiply(a, b):
+    return 0
+
+#power function, returns a to the power of b
+def pow(a,b):
+    return 0
+
+#function that solves a string equation recieved
+def solve_equation(equation):
+    return "x = "
+
+#function that recieves an equation, "Y = X + 2", and an X, and returns Y
+def solve_y(equation, y):
+    return 0
