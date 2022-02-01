b735043010a8cc8a68982f695cf742fd80641449
josephchait <danchait10@gmail.com>
2022-02-01 11:01:21 -0500

    added gui

---
+++ Python Calculator/main.py
@@ -0,0 +1,16 @@
+#this is the main function
+
+from tkinter import *
+
+# declare the window
+window = Tk()
+
+# set window title
+window.title("DS Calculator")
+
+# set window width and height
+window.configure(width=500, height=500)
+
+# set window background color
+window.configure(bg='lightgray')
+window.mainloop()
